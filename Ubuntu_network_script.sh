4a28597021a6081f27947f1afb2454f7a420db48
abelmechegia <abelmechegia@gmail.com>
2025-03-09 17:13:18 -0400

    Add files via upload

--- Ubuntu_network_script.sh
+++ Ubuntu_network_script.sh
@@ -1,25 +1,55 @@
 #!/usr/bin/bash
+#Get current date and time
+timestamp=$(date +"%Y-%m-%d_%H-%M-%S")
+filename="Ubuntu_network_script_${timestamp}.txt"
+
+#A function to collect network info.
+
+network()  { 
+	echo "Ubuntu Network information - ${timestamp}"} > ${filename}
+	echo "-----------------------------------------" >> ${filename}
 
 
-echo "IP address:   "
-ip a | grep "inet" | awk '{print $2}'
+# Display IP address
+echo "IP address:" >> ${filename}
+ip a | grep "inet" | awk '{print $2}' >> ${filename}
+echo >> ${filename}
+
+
+# Display DNS Info
+echo "DNS server:" >> ${filename}
+cat /etc/resolv.conf | grep "nameserver" >> ${filename}
+echo >> ${filename}
 
 
 
-echo "DNS server:  "
-cat /etc/resolv.conf | grep "nameserver"
+#Show Open ports
+echo "Open Ports on the server:" >> ${filename}
+sudo netstat -tuln | grep LISTEN >> ${filename}
+echo >> ${filename}
 
 
 
-echo "Open Ports on the server:    "
-sudo netstat -tuln | grep LISTEN
+
+#Display route info
+echo "Routing Table:" >> ${filename}
+ip route >> ${filename}
+echo >> ${filename}
 
 
 
-echo "Routing Table:   "
-ip route
+#Display the active interfaces
+echo "Network Interfaces: " >> ${filename}
+ip link show >> ${filename}
+echo >> ${filename}
 
+echo "Network information file created and saved to ${filename}"
 
+}
 
-echo "Network Interfaces:    "
-ip link show
+#Run the function (network) to gather the network information above
+
+network
+
+#Display the output on the screen
+cat ${filename}
